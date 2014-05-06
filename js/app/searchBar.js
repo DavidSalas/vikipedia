312f02d3abf8371487dea78b316fc4c3e25c85b8
david <davidsalas.work@gmail.com>
2014-05-05 22:00:43 -0600

    initial commit. 3rd party libs

---
+++ js/app/searchBar.js
@@ -0,0 +1,49 @@
+$(document).ready(function ()
+{
+  /*
+   * Check for enter pressed on search bar.
+   * This will probably be changed so that we
+   * can submit the form by clicking on links
+   * as well as pressing enter. Might wanna
+   * look at how to do jquery binds for this kinda thing.
+   */
+  document.getElementById("searchInput").addEventListener
+  (
+    "keydown",
+    function(e)
+    {
+      // Enter is pressed
+      if (e.keyCode == 13)
+      {
+        submitQuery($("#searchInput").val());
+      }
+    }, false
+  );
+
+  /*
+   *Search the wiki api for a list of suggested articles
+   *based on the contents of the search box.
+   */
+  $("#searchInput").autocomplete
+  ({
+    source: function(request, response)
+    {
+      $.ajax
+      ({
+        url: "http://en.wikipedia.org/w/api.php",
+        dataType: "jsonp",
+        data:
+        {
+          'action': "opensearch",
+          'format': "json",
+          'search': request.term
+        },
+        success: function(data)
+        {
+          response(data[1]);
+        }
+      });
+    }
+  });
+});
+
