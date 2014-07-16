7afa420c6512948bdacb60b3b045a0fc73414675
david <davidsalas.work@gmail.com>
2014-07-15 18:58:04 -0600

    cleanup

---
+++ test/spec/searchUtilSpecs.js
@@ -0,0 +1,14 @@
+
+var searchUtil = require("../../src/js/app/searchUtil")
+
+describe("Hello World", function() {
+
+  it("should return hello world.", function() {
+    expect(searchUtil.helloWorld()).toEqual('Hello World');
+  });
+
+  it("should make a request to the wiki api.", function(){
+    expect();
+  });
+});
+
